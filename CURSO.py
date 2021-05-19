a38b2c8c9d1c568e0c7891810127f6021abfc697
VictorGarzaLTI <victor.garza2334@gmail.com>
2021-05-19 12:55:17 -0500

    se creo un archivo con los datos de la clase curso

--- CURSO.py
+++ CURSO.py
@@ -4,3 +4,9 @@ class curso:
         self.__id_curso=id_curso
         self.__desc=desc 
         self.__id_emp=id_emp
+    def datoscurso(self):
+        f=open("c:\\Users\\victo\\Desktop\\ArchivosDeTexto\\curso.txt")
+        f.write (str(self.__id_curso))
+        f.write (str(self.__desc))
+        f.write (str(self.__id_emp))
+        f.close(None)
