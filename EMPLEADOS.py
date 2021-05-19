a8db46e36540726fcdc88c44cf45b2397d2108c9
VictorGarzaLTI <victor.garza2334@gmail.com>
2021-05-19 12:58:30 -0500

    se corrigieron errores de sangría

--- EMPLEADOS.py
+++ EMPLEADOS.py
@@ -1,14 +1,14 @@
 class empleados:
         def __init__(self, id_empleado, nom, dir):
-        self.__id_empleado = id_empleado
-        self.__nom = nom 
-        self.__dir = dir
+            self.__id_empleado = id_empleado
+            self.__nom = nom 
+            self.__dir = dir
 
         def datosemp(self):
-        f=open("c:\\Users\\victo\\Desktop\\ArchivosDeTexto\\empleados.txt")
-        f.write (str(self.__id_empleado))
-        f.write (str(self.__nom))
-        f.write (str(self.__dir))
-        f.close(None)
+            f=open("c:\\Users\\victo\\Desktop\\ArchivosDeTexto\\empleados.txt")
+            f.write (str(self.__id_empleado))
+            f.write (str(self.__nom))
+            f.write (str(self.__dir))
+            f.close(None)
 
         
