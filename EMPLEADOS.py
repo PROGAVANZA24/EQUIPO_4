97c5fa1229e402e4768e3e1738f4ee728f68d547
VictorGarzaLTI <victor.garza2334@gmail.com>
2021-05-19 12:07:32 -0500

    se crea un archivo de texo sobre los empleados

--- EMPLEADOS.py
+++ EMPLEADOS.py
@@ -5,4 +5,8 @@ class empleados:
         self.__dir = dir
 
         def datosemp(self):
-
+        f=open("c:\\desktop\\ArchivosDeTexto\\empleados.txt")
+        f.write (str(self.__id_empleado))
+        f.write (str(self.__nom))
+        f.write (str(self.__dir))
+        f.close(None)
