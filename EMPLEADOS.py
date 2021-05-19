7798080eca392a1d2ae3d161b4e66aeadfa54344
VictorGarzaLTI <victor.garza2334@gmail.com>
2021-05-19 12:51:31 -0500

    se actualizo la ruta de creacion del archivo de texto

--- EMPLEADOS.py
+++ EMPLEADOS.py
@@ -5,8 +5,10 @@ class empleados:
         self.__dir = dir
 
         def datosemp(self):
-        f=open("c:\\desktop\\ArchivosDeTexto\\empleados.txt")
+        f=open("c:\\Users\\victo\\Desktop\\ArchivosDeTexto\\empleados.txt")
         f.write (str(self.__id_empleado))
         f.write (str(self.__nom))
         f.write (str(self.__dir))
         f.close(None)
+
+        
