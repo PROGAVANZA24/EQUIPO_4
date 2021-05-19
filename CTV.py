87a72a624094ecd86804841dbb55463592396d46
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 18:54:49 -0500

    Creación metodo datoscusotemvid

--- CTV.py
+++ CTV.py
@@ -3,3 +3,10 @@ class CursoTemaVideo:
         self.__id_cursotem = id_cursotem 
         self.__id_cursotemvid = id_cursotemvid
         self.__id_video = id_video
+    def datoscursotemvid(self):
+        f=open("cursotemavideo.txt")
+        f.write (str(self.__id_cursotem))
+        f.write (str(self.__id_cursotemvid))
+        f.write(str(self.__id_video))
+        f.close(None)
+    
