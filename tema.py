ec6c9c1df75b43164c50ac75c840611a449bf2ed
Alexander Uribe <alexander_vu02@hotmail.com>
2021-05-19 17:35:48 -0500

    Se corrigio un error de sangria

--- tema.py
+++ tema.py
@@ -14,7 +14,7 @@ class tema:
     def ConsultaID(cls):
         preg=input("Escribe el id que quieres buscar: ")
         bloc=open("tema.txt") 
-    for formato in bloc:
+        for formato in bloc:
             datos=formato.strip().split("|")
             if datos[0]==preg:
                 print(datos)
