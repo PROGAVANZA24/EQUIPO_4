4da6fab376bfd3bcb91c96f6431171f2077eb93b
Erick Alan Carrizales Buentello <erickalancarrizalesbuentello@gmail.com>
2021-05-19 17:54:37 -0500

    Se definio el metodo guardar

--- CURSO_TEMA.py
+++ CURSO_TEMA.py
@@ -3,3 +3,9 @@ class CursoTema:
         self.__id_cursotem=id_cursotem
         self.__id_curso=id_curso
         self.__id_tema=id_tema
+    def guardar(self):
+        f=open("cursotema.txt")
+        f.write (str(self.__id_cursotem))
+        f.write (str(self.__id_curso))
+        f.write(str(self.__id_tema))
+        f.close(None)
