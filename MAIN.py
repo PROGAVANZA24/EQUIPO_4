0423236b7f6014472ac21e60494bb28b7e0f3c0e
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 18:19:32 -0500

    Definir función menu_admin para reutilizar el menú, definir función accion_admin

--- MAIN.py
+++ MAIN.py
@@ -1,11 +1,15 @@
 #Menú administrador
-opcion = 0
-while opcion < 1 or opcion > 6:    
-    print("*"*26+"MENÚ ADMINISTRACIÓN"+"*"*26)
-    print("1. Empleados")
-    print("2. Cursos")
-    print("3. Temas")
-    print("4. Videos")
-    print("5. Curso tema")
-    print("6. Curso tema video")
-    opcion = int(input("Ingrese opción por administrar: "))
+def menu_admin():
+    opcion = 0
+    while opcion < 1 or opcion > 6:    
+        print("*"*26+"MENÚ ADMINISTRACIÓN"+"*"*26)
+        print("1. Empleados")
+        print("2. Cursos")
+        print("3. Temas")
+        print("4. Videos")
+        print("5. Curso tema")
+        print("6. Curso tema video")
+        opcion = int(input("Ingrese opción por administrar: "))
+
+def accion_admin():
+    pass
