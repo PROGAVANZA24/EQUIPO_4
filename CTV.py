dcbb875b2894428dc9d865cd2bb3c1ffddb89dcb
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 18:53:58 -0500

    Definición atributos de la clase

--- CTV.py
+++ CTV.py
@@ -1,1 +1,5 @@
 class CursoTemaVideo:
+    def _init_(self, id_cursotem, id_cursotemvid,id_video):
+        self.__id_cursotem = id_cursotem 
+        self.__id_cursotemvid = id_cursotemvid
+        self.__id_video = id_video
