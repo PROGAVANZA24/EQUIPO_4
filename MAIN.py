0efce056864096809ad3941c8e162b28cd67692d
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 18:01:31 -0500

    Modificación bloque principal (ciclo while)

--- MAIN.py
+++ MAIN.py
@@ -1,9 +1,11 @@
 #Menú administrador
-print("*"*26+"MENÚ ADMINISTRACIÓN"+"*"*26)
-print("1. Empleados")
-print("2. Cursos")
-print("3. Temas")
-print("4. Videos")
-print("5. Curso tema")
-print("6. Curso tema video")
-opcion = int(input("Ingrese opción por administrar: "))
+opcion = 0
+while opcion < 1 or opcion > 6:    
+    print("*"*26+"MENÚ ADMINISTRACIÓN"+"*"*26)
+    print("1. Empleados")
+    print("2. Cursos")
+    print("3. Temas")
+    print("4. Videos")
+    print("5. Curso tema")
+    print("6. Curso tema video")
+    opcion = int(input("Ingrese opción por administrar: "))
