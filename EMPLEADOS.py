3d44f2d7343d365a24a0ac534dfb9375b3eaaf4a
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 20:43:50 -0500

    Modificación escritura del documento

--- EMPLEADOS.py
+++ EMPLEADOS.py
@@ -4,8 +4,8 @@ class empleados:
             self.__nom = nom 
             self.__dir = dir
         def guardar(self):
-            f=open("empleados.txt", "w")
-            f.write(f'{str(self.__id_empleado)} | {str(self.__nom)} | {str(self.__dir)}')
+            f=open("empleados.txt", "a")
+            f.write(f'{str(self.__id_empleado)} | {str(self.__nom)} | {str(self.__dir)}\n')
             f.close()
         def ConsultarTodo():
             bloc=open("empleados.txt", "r")
