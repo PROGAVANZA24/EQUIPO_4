4856612573bd7da798da74904161a695d3b5d86c
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 19:42:17 -0500

    Modificación rutas para el documento de texto

--- CURSO.py
+++ CURSO.py
@@ -5,16 +5,16 @@ class curso:
         self.__desc=desc 
         self.__id_emp=id_emp
     def guardar(self):
-        f=open("cursos.txt")
+        f=open("cursos.txt", "w")
         f.write(f'{str(self.__id_curso)} | {str(self.__desc)} | {str(self.__id_emp)}')
         f.close()
     def ConsultarTodo(cls):
-        bloc=open("cursos.txt")
+        bloc=open("cursos.txt", "r")
         print(bloc.read())
         bloc.close()
     def ConsultaID(cls):
             preg=input("Escribe el id que quieres buscar: ")
-            bloc=open("cursos.txt")
+            bloc=open("cursos.txt", "r")
             for formato in bloc:
                 datos=formato.strip().split("|")
                 if datos[0]==preg:
