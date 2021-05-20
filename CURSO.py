dddfdbd4e6fb67168a872b67277a3bf4837d12c2
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 19:39:32 -0500

    Modificación metodo guardar

--- CURSO.py
+++ CURSO.py
@@ -5,18 +5,16 @@ class curso:
         self.__desc=desc 
         self.__id_emp=id_emp
     def guardar(self):
-        f=open("c:\\Users\\victo\\Desktop\\ArchivosDeTexto\\curso.txt")
-        f.write (str(self.__id_curso))
-        f.write (str(self.__desc))
-        f.write (str(self.__id_emp))
-        f.close(None)
+        f=open("cursos.txt")
+        f.write(f'{str(self.__id_curso)} | {str(self.__desc)} | {str(self.__id_emp)}')
+        f.close()
     def ConsultarTodo(cls):
-        bloc=open("c:\\Users\\victo\\Desktop\\ArchivosDeTexto\\curso.txt")
+        bloc=open("cursos.txt")
         print(bloc.read())
         bloc.close()
     def ConsultaID(cls):
             preg=input("Escribe el id que quieres buscar: ")
-            bloc=open("c:\\Users\\victo\\Desktop\\ArchivosDeTexto\\curso.txt")
+            bloc=open("cursos.txt")
             for formato in bloc:
                 datos=formato.strip().split("|")
                 if datos[0]==preg:
