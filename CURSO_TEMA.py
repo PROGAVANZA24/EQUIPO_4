6188e24fe1b498825603639f75ea453b53016f25
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 20:46:04 -0500

    Modificación metodo ConsultarID

--- CURSO_TEMA.py
+++ CURSO_TEMA.py
@@ -4,8 +4,8 @@ class CursoTema:
         self.__id_curso=id_curso
         self.__id_tema=id_tema
     def guardar(self):
-        f=open("cursotema.txt", "w")
-        f.write(f'{str(self.__id_cursotem)} | {str(self.__id_curso)} | {str(self.__id_tema)}')
+        f=open("cursotema.txt", "a")
+        f.write(f'{str(self.__id_cursotem)} | {str(self.__id_curso)} | {str(self.__id_tema)}\n')
         f.close()
     def ConsultarTodo(cls):
         f=open("cursotema.txt", "r")
@@ -15,7 +15,7 @@ class CursoTema:
         preg=input("Escribe el id que quieres buscar: ")
         bloc=open("cursotema.txt", "r")
         for formato in bloc:
-            datos=formato.strip().split("|")
+            datos=formato.strip().split(" | ")
             if datos[0]==preg:
-                print(datos)
+                print(f'ID Tema asignado: {datos[0]} | ID Curso: {datos[1]} | ID Tema: {datos[2]}')
         bloc.close()
