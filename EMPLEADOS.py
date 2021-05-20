685152c148187c0964b2a315bc464f507843e180
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 19:11:13 -0500

    Modificación de ruta del documento de texto

--- EMPLEADOS.py
+++ EMPLEADOS.py
@@ -4,18 +4,18 @@ class empleados:
             self.__nom = nom 
             self.__dir = dir
         def guardar(self):
-            f=open("c:\\Users\\victo\\Desktop\\ArchivosDeTexto\\empleados.txt")
+            f=open("empleados.txt")
             f.write (str(self.__id_empleado))
             f.write (str(self.__nom))
             f.write (str(self.__dir))
             f.close(None)
         def ConsultarTodo(cls):
-            bloc=open("c:\\Users\\victo\\Desktop\\ArchivosDeTexto\\empleados.txt")
+            bloc=open("empleados.txt")
             print(bloc.read())
             bloc.close()
         def ConsultaID(cls):
             preg=input("Escribe el id que quieres buscar: ")
-            bloc=open("c:\\Users\\victo\\Desktop\\ArchivosDeTexto\\empleados.txt")
+            bloc=open("empleados.txt")
             for formato in bloc:
                 datos=formato.strip().split("|")
                 if datos[0]==preg:
