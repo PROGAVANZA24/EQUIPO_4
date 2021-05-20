1cb836de6c12e3b35e816d57a68003bdf25ae79d
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 20:12:33 -0500

    Modificación metodo guardar

--- VIDEOS.py
+++ VIDEOS.py
@@ -3,14 +3,11 @@ class video:
         self.__id_video=id_video
         self.__nom=nom
         self.__url=url
-        self.__publi=publi 
+        self.__publi=publi
     def guardar(self):
-        f=open("video.txt")
-        f.write (str(self.__id_video))
-        f.write (str(self.__nom))
-        f.write(str(self.__url))
-        f.write(str(self.__publi))
-        f.close(None)
+        f=open("videos.txt", "w")
+        f.write(f'{str(self.__id_video)} | {str(self.__nom)} | {str(self.__url)} | {str(self.__publi)}')
+        f.close()
     def ConsularTodo(cls):
         f=open("video.txt")
         print(f.read())
