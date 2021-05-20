c3bed945d8fba4aa6a8ec367d5468077f89bd1b6
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-19 21:01:56 -0500

    Modificación del constructor de la clase

--- CTV.py
+++ CTV.py
@@ -1,5 +1,5 @@
 class CursoTemaVideo:
-    def _init_(self, id_cursotem, id_cursotemvid,id_video):
+    def __init__(self, id_cursotem, id_cursotemvid,id_video):
         self.__id_cursotem = id_cursotem 
         self.__id_cursotemvid = id_cursotemvid
         self.__id_video = id_video
